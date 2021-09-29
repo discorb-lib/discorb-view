7ce6950843c006a10c6e9d5b8ed3157aee2511d1
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-09-29 18:49:13 +0900

    Add: Add menu example

---
+++ examples/menu.rb
@@ -0,0 +1,47 @@
+require "discorb"
+require "discorb/view"
+
+client = Discorb::Client.new
+
+client.extend Discorb::View::Extension
+
+class MyMenu
+  extend Discorb::View::Base
+
+  @@texts = ["A", "B"]
+
+  def initialize
+    @page = 0
+  end
+
+  button :left, "<", :primary do |interaction|
+    @page -= 1
+  end
+
+  button :right, ">", :primary do |interaction|
+    @page += 1
+  end
+
+  button :quit, "Quit", :danger do |interaction|
+    stop!
+  end
+
+  view do |result|
+    result.content = @@texts[@page % @@texts.length]
+    result.embeds = []
+    result.components = [:left, :right, :quit]
+  end
+end
+
+client.once :standby do
+  puts "Ready!"
+  MyMenu.start(client.channels["875268321652641792"])
+end
+
+client.on :message do |message|
+  next unless message.content == "menu"
+
+  MyMenu.start(message.channel)
+end
+
+client.run ENV["DISCORD_BOT_TOKEN"]
